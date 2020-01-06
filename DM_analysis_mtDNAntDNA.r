111d2ed86125d41bd12cee6e32781b2940e2b151
Antons <antony.suvorov@gmail.com>
2020-01-05 21:24:02 -0500

    update

--- DM_analysis_mtDNAntDNA.r
+++ DM_analysis_mtDNAntDNA.r
@@ -86,17 +86,3 @@ fit=run.jags(
             )
 save(fit, file = "MCMC_mtDNAntDNA.RData")
 
-
-#Test simulation
-mydata=list(
-                y=t(cbind(rmultinom(10,size=rpois(10,100),prob=c(0.15,0.1,0.7,0.05)),
-                    rmultinom(10,size=rpois(10,100),prob=c(0.1,0.1,0.1,0.7)),
-                    rmultinom(10,size=rpois(10,100),prob=c(0.6,0.3,0.05,0.05)))),
-                K=apply(y,1,sum),
-                Nsample=nrow(y),
-                Notu=ncol(y),
-                regions=rep(c(1,2,3),each=10),
-                Nregion=length(unique(regions))
-                #mtdna=sample(c(1,2),size=30,replace=T),
-                #Nmtdna=length(unique(mtdna))
-    )
